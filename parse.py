5a2a5d17547b77901294243ead95d03f814bad6f
Sergey Petrov <sergey.petrov@vdombox.ru>
2014-08-25 01:25:23 +0700

    - Removed: unnecessary print

--- parse.py
+++ parse.py
@@ -1246,7 +1246,6 @@ def main():
         ignore = json_load(ignore, critical=True)
         INFO("Done: 'ignore' configuration file")
 
-    print os.path.split(args.source.name)
     config = {
         "target": {
             "path": args.target or os.path.split(args.source.name)[-1].split(".")[0],
